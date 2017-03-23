2feb339f85382a53aae978bfc8eb6a7c4d586dfe
zhaoyafeiwork <zhaoyafeiwork@sina.com>
2017-03-23 12:11:28 +0800

    提交测试

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ss");
-        System.out.println("ss");
+        System.out.println("ssssss");
+        System.out.println("ssssssfhh");
         System.out.println("Hello World!");
     }
 }
